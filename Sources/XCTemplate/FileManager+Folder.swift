726fd99d6164078ccfe02272b56445a7a8b60bfc
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2020-05-03 13:06:54 +0200

    Clean file manager url

--- Sources/XCTemplate/FileManager+Folder.swift
+++ Sources/XCTemplate/FileManager+Folder.swift
@@ -17,22 +17,22 @@ extension FileManager {
 
     func url(for folder: Folder) -> URL {
         let url: URL
-        let home = homeDirectoryForCurrentUser
         switch folder {
         case .xcodeDestination:
-            url = URL(
-                fileURLWithPath: "Library/Developer/Xcode/Templates",
-                relativeTo: home
-            )
+            url = xcodeTemplateURL
         case let .templates(namespace):
-            url = URL(
-                fileURLWithPath: "Library/Developer/Xcode/Templates",
-                relativeTo: home
-            ).appendingPathComponent(namespace)
+            url = xcodeTemplateURL.appendingPathComponent(namespace)
         case .workingDirectory:
-            url = temporaryDirectory.appendingPathComponent("XCTemplateInstaller")
+            url = temporaryDirectory.appendingPathComponent("XCTemplate-CLI")
         }
         try? createDirectory(at: url, withIntermediateDirectories: true)
         return url
+    }
+
+    private var xcodeTemplateURL: URL {
+        URL(
+            fileURLWithPath: "Library/Developer/Xcode/Templates",
+            relativeTo: homeDirectoryForCurrentUser
+        )
     }
 }
